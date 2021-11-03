f94f297a5fe622983ed844f245d9b17bea959226
njokicynthia590@gmail.com <njokicynthia590@gmail.com>
2021-11-03 20:45:33 +0300

    Added login controller file

--- resources/js/components/examplecomponent.vue
+++ resources/js/components/examplecomponent.vue
@@ -69,6 +69,6 @@
         
           e.preventDefault();
         }
-      }
+        }
     }
 </script>
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/vuelogin/',  'Auth\LoginController@vuelogin')->name('vuelogin');
 
  Route::get('/vuelogin', function () {
-    return view('welcome');
+    return view('vuelogin');
 
    
 });
